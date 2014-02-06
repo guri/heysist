0a7a803302bc28cb14ccd54966e18f74a05ba559
galia <smartgit@galiaba.com>
2014-02-06 21:58:46 +0200

    status page now switches between sections

--- scripts/common.js
+++ scripts/common.js
@@ -5,5 +5,18 @@ $(document).ready(function() {
 			.text('Heyssist')
 			.buttonMarkup({theme: 'b'})
 	);
+	
+	
+	function sectionSwitch(sectionObj) {
+		// TODO: make more specific!
+		$('section').hide();
+		sectionObj.show();
+		
+	}
+	sectionSwitch($('section').eq(0));
+	$('body#status').find('nav a').click(function(){
+		sectionSwitch($('#'+$(this).data('triggers')));
+	});
+	
 
 })
