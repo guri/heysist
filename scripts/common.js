b243ad1b4cc36f0f7a7a5de4b18551277f187ea0
galia <smartgit@galiaba.com>
2014-02-07 04:07:51 +0200

    Registration works with firebase. Input validation and handling returning users is not implemented yet.

--- scripts/common.js
+++ scripts/common.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-
+	
+	/* Title */
 	$('[data-role="page"]').prepend(
 		$('<h2 id="site-title">')
 			.text('Heyssist')
@@ -7,6 +8,8 @@ $(document).ready(function() {
 	);
 	
 	
+	
+	/* Status page flip sections */
 	function sectionSwitch(sectionObj) {
 		// TODO: make more specific!
 		$('section').hide();
@@ -17,6 +20,33 @@ $(document).ready(function() {
 	$('body#status').find('nav a').click(function(){
 		sectionSwitch($('#'+$(this).data('triggers')));
 	});
+
+
+	
+	/* ACTIONS */
+	
+	$('#registration_form').submit(function(){
+		var form = $(this);
+		var user = {};
+		if (user=='')
+			alert('Enter User Name'); // TODO!!
+
+		// text fields
+		$.each(['name', 'location', 'phone', 'abilities-other', 'needs-other'], function(n, key){
+			var val = form.find('#'+key).val();
+			if (val !== undefined)
+				user[key] = val;
+		});
+
+		// checkboxes
+		$(':checked').each(function(n, obj){
+			var key = $(obj).attr('name');
+			user[key] = true;
+		})
+		insertUser(user);
+		return false;
+	});
+	
 	
 
 })
